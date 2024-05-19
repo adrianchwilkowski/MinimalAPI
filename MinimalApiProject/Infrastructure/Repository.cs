3a71e94465610d4e60ef4d2aa1c16e465d596254
Adrian Chwiłkowski <adrian.chwilkowski@i4b.pl>
2024-05-19 12:50:03 +0200

    Add project files.

---
+++ MinimalApiProject/Infrastructure/Repository.cs
@@ -0,0 +1,12 @@
+﻿namespace MinimalApiProject.Infrastructure
+{
+    public class Repository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
