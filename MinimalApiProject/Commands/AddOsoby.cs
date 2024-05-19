14d95e4de3017adadc404fd9ef0ef747b9da731c
Adrian Chwiłkowski <adrian.chwilkowski@i4b.pl>
2024-05-19 13:10:10 +0200

    added AddOsoby command

---
+++ MinimalApiProject/Commands/AddOsoby.cs
@@ -0,0 +1,23 @@
+﻿using MinimalApiProject.Models;
+
+namespace MinimalApiProject.Commands
+{
+    public class AddOsoby
+    {
+        public string Imie { get; set; } = null!;
+        public string Nazwisko { get; set; } = null!;
+        public DateTime DataUrodzenia { get; set; }
+        public string Adres { get; set; } = null!;
+        public Osoby ToOsoby()
+        {
+            return new Osoby
+            {
+                ID = Guid.NewGuid(),
+                Imie = Imie,
+                Nazwisko = Nazwisko,
+                Adres = Adres,
+                DataUrodzenia = DataUrodzenia
+            };
+        }
+    }
+}
